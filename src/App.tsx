3cdeb80a1af528b9a36e48cd437ac649f076fc9e
Chris B <babayanchristopher@gmail.com>
2025-04-10 14:14:53 -0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import SideBar from './components/Sidebar.tsx'
+
+function App() {
+  return (
+    <div>
+      <header className='bg-gray-600'>
+        <div className=''>
+
+        </div>
+      </header>
+      <div className='flex'>
+
+        <SideBar/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
